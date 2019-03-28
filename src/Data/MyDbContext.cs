b909043f4fe5a4969b31687f5dbb09e31fc30922
dudu <dudu@cnblogs.com>
2019-03-28 16:22:42 +0800

    Add solution files

---
+++ src/Data/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EfUnexpectedDeletion.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfUnexpectedDeletion.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>()
+                .HasOne(q => q.Owner)
+                .WithOne();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using EfUnexpectedDeletion.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+using EfUnexpectedDeletion.Entities;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace EfUnexpectedDeletion
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var conn = "server=.;database=question;integrated security=true";
+            var host = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddDbContext<MyDbContext>(options => options.UseSqlServer(conn));
+                }).Build();
+
+            using (var db = host.Services.GetRequiredService<MyDbContext>())
+            {
+                var newQuestion = new Question
+                {
+                    Title = "test " + DateTime.Now.ToLongDateString(),
+                    Owner = await db.Set<User>().FirstAsync(u => u.Id == 1)
+                };
+
+                var latestQuestion = await db.Set<Question>()
+                    .Where(q => q.UserId == 1).OrderByDescending(q => q.Id).FirstOrDefaultAsync();
+
+                db.Set<Question>().Add(newQuestion);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
